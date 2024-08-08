3e4d8b0d824db34ff697212b08c7890a80e9b971
Lawrence <34848993+lawrence415610@users.noreply.github.com>
2024-08-08 18:44:40 +1000

    Initial commit

---
+++ Projects/GuessNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int result = Random.Shared.Next(1, 101);
+while(true){
+    Console.Write("Guess a number");
+    int userInput = int.Parse(Console.ReadLine() ?? "");
+    if(userInput == result) break;
+    if(userInput > result){
+        Console.WriteLine("Too big");
+    }else{
+        Console.WriteLine("Too Small");
+    }
+}
+Console.WriteLine("The result is " + result);
