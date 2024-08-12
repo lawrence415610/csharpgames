86356affa448009b846e2208d5f4ebde11c2f9fe
Lawrence <34848993+lawrence415610@users.noreply.github.com>
2024-08-12 11:54:56 +1000

    update the first program

--- Projects/GuessNumber/Program.cs
+++ Projects/GuessNumber/Program.cs
@@ -1,13 +1,19 @@
-﻿
-int result = Random.Shared.Next(1, 101);
-while(true){
-    Console.Write("Guess a number");
-    int userInput = int.Parse(Console.ReadLine() ?? "");
-    if(userInput == result) break;
-    if(userInput > result){
-        Console.WriteLine("Too big");
-    }else{
-        Console.WriteLine("Too Small");
+﻿int result = Random.Shared.Next(1, 101);
+while (true)
+{
+    Console.WriteLine("Guess a number: ");
+    if (!int.TryParse(Console.ReadLine(), out int userInput))
+    {
+        Console.WriteLine("Invalid input, please enter a valid integer.");
+    }
+    if (userInput == result) break;
+    if (userInput > result)
+    {
+        Console.WriteLine(userInput + " is too big");
+    }
+    else
+    {
+        Console.WriteLine(userInput + " is too small");
     }
 }
 Console.WriteLine("The result is " + result);
--- Projects/GuessNumber/obj/Debug/net8.0/GuessNumber.AssemblyInfo.cs
+++ Projects/GuessNumber/obj/Debug/net8.0/GuessNumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4d8b0d824db34ff697212b08c7890a80e9b971")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
